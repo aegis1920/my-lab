f0bc538ca0c9b5126294c33c9fa40a1a73b10e61
aegis <aegis1920@gmail.com>
2021-01-21 20:27:43 +0900

    test : 이펙티브 자바 아이템 8 학습 테스트

---
+++ effective-java/src/main/java/com/bingbong/effectivejava/item9/Reader.java
@@ -0,0 +1,28 @@
+package com.bingbong.effectivejava.item9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+// 자원 회수를 위해 finalizer와 cleaner가 아닌 AutoCloseable을 구현해서 사용하자.
+public class Reader implements AutoCloseable {
+
+    private final BufferedReader bufferedReader;
+
+    public Reader(BufferedReader bufferedReader) {
+        this.bufferedReader = bufferedReader;
+    }
+
+    public String readFirstLine() throws IOException {
+        return bufferedReader.readLine();
+    }
+
+    @Override
+    public void close() {
+        try {
+            bufferedReader.close();
+            System.out.print("closed도 실행됐다!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ effective-java/src/test/java/com/bingbong/effectivejava/item9/ReaderTest.java
@@ -0,0 +1,41 @@
+package com.bingbong.effectivejava.item9;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ReaderTest {
+
+    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(System.out);
+    }
+
+    @DisplayName("AutoCloseable로 close가 실행된다")
+    @Test
+    void autoCloseable() throws IOException {
+        BufferedReader testBufferedReader = new BufferedReader(
+            new InputStreamReader(new ByteArrayInputStream("TEST DATA".getBytes())));
+
+        try (Reader reader = new Reader(testBufferedReader)) {
+            assertThat(reader.readFirstLine()).isEqualTo("TEST DATA");
+        }
+        assertThat(outContent).hasToString("closed도 실행됐다!");
+    }
+}
