16c2dc186220c04371f7163d4fc9ca80eccfd0bd
aegis <aegis1920@gmail.com>
2021-01-29 15:26:26 +0900

    test : JPA Dto 조회 및 여러 반환 타입 조회 테스트

--- spring-data-jpa/src/main/java/com/bingbong/springdatajpa/repository/MemberRepository.java
+++ spring-data-jpa/src/main/java/com/bingbong/springdatajpa/repository/MemberRepository.java
@@ -1,10 +1,34 @@
 package com.bingbong.springdatajpa.repository;
 
 import com.bingbong.springdatajpa.domain.Member;
+import com.bingbong.springdatajpa.dto.MemberDto;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new com.bingbong.springdatajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") Collection<String> names);
+
+    List<Member> findListByUsername(String username); //컬렉션
+
+    Member findMemberByUsername(String username); //단건
+
+    Optional<Member> findOptionalByUsername(String username); //단건 Optional
+
 }
--- spring-data-jpa/src/test/java/com/bingbong/springdatajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa/src/test/java/com/bingbong/springdatajpa/repository/MemberRepositoryTest.java
@@ -3,6 +3,9 @@ package com.bingbong.springdatajpa.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.bingbong.springdatajpa.domain.Member;
+import com.bingbong.springdatajpa.domain.Team;
+import com.bingbong.springdatajpa.dto.MemberDto;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,9 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     void basicCRUD() {
@@ -58,5 +64,68 @@ class MemberRepositoryTest {
 
         assertThat(result).hasSize(1);
     }
+
+    @Test
+    void findUser() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+
+        assertThat(result).hasSize(1);
+    }
+
+    @Test
+    void findUsernameList() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> result = memberRepository.findUsernameList();
+
+        assertThat(result).hasSize(2);
+    }
+
+    @Test
+    void findMemberDto() {
+        Team team = new Team("A");
+        teamRepository.save(team);
+
+        Member m1 = new Member("AAA", 10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+        List<MemberDto> result = memberRepository.findMemberDto();
+
+        assertThat(result).hasSize(1);
+    }
+
+    @Test
+    void findByNames() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA","BBB"));
+
+        assertThat(result).hasSize(2);
+    }
+
+    @Test
+    void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result1 = memberRepository.findListByUsername("AAA");
+        Member result2 = memberRepository.findMemberByUsername("AAA");
+        Member result3 = memberRepository.findOptionalByUsername("AAA").get();
+    }
+
 
 }
