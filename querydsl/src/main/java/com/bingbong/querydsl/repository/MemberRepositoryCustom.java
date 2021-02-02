e9a206518e5180191ecea4ce2da1b584fee29787
aegis <aegis1920@gmail.com>
2021-02-02 13:01:55 +0900

    test : QueryDSL 페이징 학습테스트

--- querydsl/src/main/java/com/bingbong/querydsl/repository/MemberRepositoryCustom.java
+++ querydsl/src/main/java/com/bingbong/querydsl/repository/MemberRepositoryCustom.java
@@ -3,7 +3,11 @@ package com.bingbong.querydsl.repository;
 import com.bingbong.querydsl.dto.MemberSearchCondition;
 import com.bingbong.querydsl.dto.MemberTeamDto;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface MemberRepositoryCustom {
     List<MemberTeamDto> search(MemberSearchCondition condition);
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
 }
