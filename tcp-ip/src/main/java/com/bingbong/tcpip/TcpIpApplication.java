2b8c6ffbdde98510d706eebefbb51db5f576171f
aegis <aegis1920@gmail.com>
2021-03-24 00:14:50 +0900

    init : tcp / ip 프로그래밍 스터디 시작

---
+++ tcp-ip/src/main/java/com/bingbong/tcpip/TcpIpApplication.java
@@ -0,0 +1,13 @@
+package com.bingbong.tcpip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpIpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcpIpApplication.class, args);
+    }
+
+}
---
+++ tcp-ip/src/test/java/com/bingbong/tcpip/TcpIpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingbong.tcpip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcpIpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
