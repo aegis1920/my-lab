6d2ba2bb7479ade68e92d8f6eed27652bd6c9b7c
aegis <aegis1920@gmail.com>
2021-04-05 13:29:46 +0900

    test : Thread join 학습 테스트

--- multi-thread/src/test/java/com/bingbong/multithread/exercise/MultiThreadTest.java
+++ multi-thread/src/test/java/com/bingbong/multithread/exercise/MultiThreadTest.java
@@ -19,6 +19,61 @@ class MultiThreadTest {
         atomicSum = new AtomicInteger();
     }
 
+    @DisplayName("join을 하지 않은 thread, 계속 살아있으며 main Group")
+    @Test
+    void notJoinedThread() {
+        Thread mainThread = Thread.currentThread();
+        Thread newThread1 = new TimerThread(1);
+        Thread newThread2 = new TimerThread(1);
+
+        newThread1.start();
+        newThread2.start();
+
+        assertThat(mainThread.getId()).isGreaterThan(0L);
+        assertThat(mainThread.getName()).isEqualTo("Test worker");
+        assertThat(mainThread.getThreadGroup().getName()).isEqualTo("main");
+        assertThat(mainThread.isAlive()).isTrue();
+
+        assertThat(newThread1.getId()).isGreaterThan(0L);
+        assertThat(newThread1.getThreadGroup().getName()).isEqualTo("main");
+        assertThat(newThread1.getName()).contains("Thread-");
+        assertThat(newThread1.isAlive()).isTrue();
+
+        assertThat(newThread2.getId()).isGreaterThan(0L);
+        assertThat(newThread2.getThreadGroup().getName()).isEqualTo("main");
+        assertThat(newThread2.getName()).contains("Thread-");
+        assertThat(newThread2.isAlive()).isTrue();
+    }
+
+    @DisplayName("join한 thread, 각 스레드는 실행이 완료되면 죽고 Group이 null임")
+    @Test
+    void joinedThread() throws InterruptedException {
+        Thread mainThread = Thread.currentThread();
+        Thread newThread1 = new TimerThread(1);
+        Thread newThread2 = new TimerThread(1);
+
+        newThread1.start();
+        newThread2.start();
+
+        newThread1.join();
+        newThread2.join();
+
+        assertThat(mainThread.getId()).isGreaterThan(0L);
+        assertThat(mainThread.getName()).isEqualTo("Test worker");
+        assertThat(mainThread.getThreadGroup().getName()).isEqualTo("main");
+        assertThat(mainThread.isAlive()).isTrue();
+
+        assertThat(newThread1.getId()).isGreaterThan(0L);
+        assertThat(newThread1.getThreadGroup()).isNull();
+        assertThat(newThread1.getName()).contains("Thread-");
+        assertThat(newThread1.isAlive()).isFalse();
+
+        assertThat(newThread2.getId()).isGreaterThan(0L);
+        assertThat(newThread2.getThreadGroup()).isNull();
+        assertThat(newThread2.getName()).contains("Thread-");
+        assertThat(newThread2.isAlive()).isFalse();
+    }
+
     @DisplayName("2개의 스레드로 1억을 만들기, 실패")
     @Test
     void twoThreadSum() throws InterruptedException {
@@ -79,6 +134,24 @@ class MultiThreadTest {
     private void workerThreadWithAtomic() {
         for (int i = 0; i < 25_000_000; i++) {
             atomicSum.addAndGet(2);
+        }
+    }
+
+    static class TimerThread extends Thread {
+
+        public int count;
+
+        TimerThread(int count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(count * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
