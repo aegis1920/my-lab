f99ed2d1e75718e0a4bcb24aed7b46181268446a
aegis <aegis1920@gmail.com>
2021-06-17 02:00:39 +0900

    init : [SpringRunner] Spring POJO

---
+++ coffeehouse/src/main/java/com/bingbong/coffeehouse/CoffeehouseApplication.java
@@ -0,0 +1,9 @@
+package com.bingbong.coffeehouse;
+
+
+public class CoffeehouseApplication {
+	
+	public static void main(String[] args) {
+	}
+	
+}
---
+++ coffeehouse/src/test/java/com/bingbong/coffeehouse/CoffeehouseApplicationTests.java
@@ -0,0 +1,11 @@
+package com.bingbong.coffeehouse;
+
+import org.junit.jupiter.api.Test;
+
+class CoffeehouseApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
