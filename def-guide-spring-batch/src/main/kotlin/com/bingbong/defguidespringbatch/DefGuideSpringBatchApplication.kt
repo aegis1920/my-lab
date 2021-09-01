563807ef3c035a19f8ecc9be8058ecfd995f0d58
aegis <aegis1920@gmail.com>
2021-09-01 22:38:33 +0900

    init : Spring Batch 완벽 가이드 proj

---
+++ def-guide-spring-batch/src/main/kotlin/com/bingbong/defguidespringbatch/DefGuideSpringBatchApplication.kt
@@ -0,0 +1,11 @@
+package com.bingbong.defguidespringbatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DefGuideSpringBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<DefGuideSpringBatchApplication>(*args)
+}
---
+++ def-guide-spring-batch/src/test/kotlin/com/bingbong/defguidespringbatch/DefGuideSpringBatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bingbong.defguidespringbatch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DefGuideSpringBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
