e4180399a2c4e697b9382428f2f16dc299cd7c2d
aegis <aegis1920@gmail.com>
2021-09-01 23:50:58 +0900

    add : HelloWorldJob Step2 추가

--- def-guide-spring-batch/src/main/kotlin/com/bingbong/defguidespringbatch/jobs/HelloWorldJob.kt
+++ def-guide-spring-batch/src/main/kotlin/com/bingbong/defguidespringbatch/jobs/HelloWorldJob.kt
@@ -5,6 +5,8 @@ import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -22,6 +24,7 @@ class HelloWorldJob(
     fun job(): Job {
         return this.jobBuilderFactory.get("basicJob")
             .start(step1())
+            .next(step2())
             .build()
     }
 
@@ -32,6 +35,25 @@ class HelloWorldJob(
                 println("Hello World!")
                 RepeatStatus.FINISHED
             }.build()
+    }
+
+    @Bean
+    fun step2(): Step {
+        return this.stepBuilderFactory.get("step2")
+            .tasklet(helloWorldTasklet())
+            .build()
+    }
+
+    @Bean
+    fun helloWorldTasklet(): Tasklet {
+        // stepContribution은 아직 커밋되지 않은 현재 트랜잭션에 대한 정보
+        // chunkContext는 실행 시점의 Job 상태 or Chunk와 관련된 정보. Job에 관련된 정보도 있
+        return Tasklet { _, chunkContext: ChunkContext ->
+            val name = chunkContext.stepContext
+                .jobParameters["name"]
+            println("Hello ${name}!")
+            RepeatStatus.FINISHED
+        }
     }
 }
 
