79de1ebdb951df1ee69b84b43e46d3a17c221def
aegis <aegis1920@gmail.com>
2021-09-02 00:11:47 +0900

    add : HelloWorldJob Step3 추가

--- def-guide-spring-batch/src/main/kotlin/com/bingbong/defguidespringbatch/jobs/HelloWorldJob.kt
+++ def-guide-spring-batch/src/main/kotlin/com/bingbong/defguidespringbatch/jobs/HelloWorldJob.kt
@@ -5,10 +5,12 @@ import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.core.scope.context.ChunkContext
 import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -25,6 +27,7 @@ class HelloWorldJob(
         return this.jobBuilderFactory.get("basicJob")
             .start(step1())
             .next(step2())
+            .next(step3())
             .build()
     }
 
@@ -45,13 +48,30 @@ class HelloWorldJob(
     }
 
     @Bean
+    fun step3(): Step {
+        return this.stepBuilderFactory.get("step3")
+            .tasklet(helloWorldTaskletLateBiding(null)) // 늦게 binding 되기 때문에 null을 넣어준다.
+            .build()
+    }
+
+    @Bean
     fun helloWorldTasklet(): Tasklet {
         // stepContribution은 아직 커밋되지 않은 현재 트랜잭션에 대한 정보
-        // chunkContext는 실행 시점의 Job 상태 or Chunk와 관련된 정보. Job에 관련된 정보도 있
+        // chunkContext는 실행 시점의 Job 상태 or Chunk와 관련된 정보. Job에 관련된 정보도 있다
         return Tasklet { _, chunkContext: ChunkContext ->
             val name = chunkContext.stepContext
                 .jobParameters["name"]
             println("Hello ${name}!")
+            RepeatStatus.FINISHED
+        }
+    }
+
+    // 이 스코프를 사용해야 이 실행 범위에 왔을 때까지 Bean 생성을 지연시켜줄 수 있다. 그래서 잡 파라미터를 빈 생성 시점에 주입시켜줄 수 있다.
+    @StepScope
+    @Bean
+    fun helloWorldTaskletLateBiding(@Value("#{jobParameters['name']}") name: String?): Tasklet {
+        return Tasklet { _, _ ->
+            println("Hello ${name}!!!!!!!!!!!!!!!!!!!!!")
             RepeatStatus.FINISHED
         }
     }
