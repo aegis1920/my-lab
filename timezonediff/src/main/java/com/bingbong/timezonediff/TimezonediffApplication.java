099a94e37a8d2023c7c073aac57801f9cdf17fda
aegis <aegis1920@gmail.com>
2022-08-04 00:46:08 +0900

    timezonediff proj init

---
+++ timezonediff/src/main/java/com/bingbong/timezonediff/TimezonediffApplication.java
@@ -0,0 +1,13 @@
+package com.bingbong.timezonediff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimezonediffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimezonediffApplication.class, args);
+    }
+
+}
---
+++ timezonediff/src/test/java/com/bingbong/timezonediff/TimezonediffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingbong.timezonediff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimezonediffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
